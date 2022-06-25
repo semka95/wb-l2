2c6259d3eeed17039b0ba9a7b2248bd10067929b
Semyon Zernov <sem.z@protonmail.com>
2022-06-25 15:23:03 +0300

    [dev04]: solve task

--- develop/dev04/main.go
+++ develop/dev04/main.go
@@ -1,5 +1,3 @@
-package main
-
 /*
 === Поиск анаграмм по словарю ===
 
@@ -19,6 +17,77 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+package main
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func unorderedEqual(str1, str2 string) bool {
+	if len(str1) != len(str2) {
+		return false
+	}
+
+	exists := make(map[rune]struct{})
+	for _, v := range str1 {
+		exists[v] = struct{}{}
+	}
+	for _, v := range str2 {
+		if _, ok := exists[v]; !ok {
+			return false
+		}
+	}
+
+	return true
+}
+
+func anagram(data []string) map[string][]string {
+	res := make(map[string][]string)
+	exists := make(map[string]struct{})
+
+	for i, v := range data {
+		for j := i + 1; j < len(data); j++ {
+			if _, ok := exists[data[j]]; ok {
+				continue
+			}
+
+			if unorderedEqual(v, data[j]) {
+				res[v] = append(res[v], data[j])
+				exists[data[j]] = struct{}{}
+				exists[v] = struct{}{}
+			}
+		}
+	}
+
+	return res
+}
+
+func prepareData(data []string) []string {
+	res := make([]string, 0, len(data))
+	exists := make(map[string]struct{})
+
+	for _, v := range data {
+		str := strings.ToLower(v)
+
+		if _, ok := exists[str]; ok {
+			continue
+		}
+
+		res = append(res, str)
+		exists[str] = struct{}{}
+	}
+
+	sort.Strings(res)
+
+	return res
+}
+
+func main() {
+	data := []string{"пятка", "пятак", "тяпка", "листок", "столик", "столик", "СЛИТОК"}
+	data = prepareData(data)
+	res := anagram(data)
+
+	fmt.Println(res)
 }
