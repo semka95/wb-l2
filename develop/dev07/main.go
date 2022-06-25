d58be3f209cdb0432d2ebeea0d1f478dc4603beb
Semyon Zernov <sem.z@protonmail.com>
2022-06-25 16:20:32 +0300

    [dev07]: solve task

--- develop/dev07/main.go
+++ develop/dev07/main.go
@@ -1,5 +1,3 @@
-package main
-
 /*
 === Or channel ===
 
@@ -31,8 +29,58 @@ start := time.Now()
 )
 
 fmt.Printf(“fone after %v”, time.Since(start))
+
+https://go.dev/blog/pipelines
 */
+package main
+
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("exit after %v\n", time.Since(start))
+
+}
+
+func or[T any](channels ...<-chan T) <-chan T {
+	out := make(chan T)
+	exitchan := make(chan struct{})
+
+	output := func(c <-chan T) {
+		for n := range c {
+			out <- n
+		}
+		exitchan <- struct{}{}
+
+	}
+	for _, c := range channels {
+		go output(c)
+	}
+
+	go func() {
+		<-exitchan
+		close(out)
+	}()
+	return out
 }
