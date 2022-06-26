e3017f3045b82903063a8c0cf60485f4efe67835
Semyon Zernov <sem.z@protonmail.com>
2022-06-26 13:34:27 +0300

    fix(dev11): add info logs in server launch

--- develop/dev11/main.go
+++ develop/dev11/main.go
@@ -22,12 +22,14 @@ func main() {
 	defer logger.Sync()
 	zap.ReplaceGlobals(logger)
 
+	logger.Info("reading config")
 	config, err := NewConfig()
 	if err != nil {
 		logger.Error("can't decode config", zap.Error(err))
 		return
 	}
 
+	logger.Info("connecting to database")
 	db, err := bolt.NewBoltDB(config.DBPath)
 	if err != nil {
 		panic(err)
@@ -44,6 +46,7 @@ func main() {
 		IdleTimeout: time.Duration(config.IdleTimeout) * time.Second,
 	}
 
+	logger.Info("running http server")
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			logger.Fatal("can't start server", zap.Error(err), zap.String("server address", config.HTTPServerAddress))
@@ -54,6 +57,7 @@ func main() {
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 	<-stop
+	logger.Info("received interrupt signal, closing server")
 	timeout, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
 	defer cancel()
 	if err := srv.Shutdown(timeout); err != nil {
