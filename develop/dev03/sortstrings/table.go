e66a77b4172d654b1ea58c0553c1d29e0a0d09fa
Semyon Zernov <sem.z@protonmail.com>
2022-07-09 22:49:56 +0300

    fix(dev03): fix out of range column

--- develop/dev03/sortstrings/table.go
+++ develop/dev03/sortstrings/table.go
@@ -15,22 +15,27 @@ func (t stringTable) Len() int {
 }
 
 func (t stringTable) Less(i, j int) bool {
+	col := t.column
+	if col > len(t.data[i])-1 || col > len(t.data[j]) {
+		col = 0
+	}
+
 	if t.isNumeric {
-		n1 := trimNonNumber(t.data[i][t.column])
-		n2 := trimNonNumber(t.data[j][t.column])
+		n1 := trimNonNumber(t.data[i][col])
+		n2 := trimNonNumber(t.data[j][col])
 
 		i1, err := strconv.Atoi(n1)
 		if err != nil {
-			return (t.data[i][t.column] < t.data[j][t.column])
+			return (t.data[i][col] < t.data[j][col])
 		}
 		j1, err := strconv.Atoi(n2)
 		if err != nil {
-			return (t.data[i][t.column] < t.data[j][t.column])
+			return (t.data[i][col] < t.data[j][col])
 		}
 
 		return i1 < j1
 	}
-	return (t.data[i][t.column] < t.data[j][t.column])
+	return (t.data[i][col] < t.data[j][col])
 }
 
 func (t stringTable) Swap(i, j int) {
